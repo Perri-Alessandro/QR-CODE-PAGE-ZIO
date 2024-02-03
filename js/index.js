70d4b7e206cf999eed53abba41022820cd5a9a45
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-03 19:39:06 +0100

    audio player about ready, responsive and nice:)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-let annoCorrente = new Date().getFullYear();
-let eccolo = (document.getElementById("anno").textContent = annoCorrente);
+// let annoCorrente = new Date().getFullYear();
+// let eccolo = (document.getElementById("anno").textContent = annoCorrente);
 
 ///////////////////////////// AUDIO PLAYER
 document.addEventListener("DOMContentLoaded", function () {
-  const audioPlayer = document.getElementById("audioPlayer");
+  const playButton = document.getElementById("playButton");
+  const pauseButton = document.getElementById("pauseButton");
   const progressBar = document.getElementById("progressBar");
   const progress = document.getElementById("progress");
   const currentTime = document.getElementById("currentTime");
   const totalTime = document.getElementById("totalTime");
+
+  pauseButton.classList.add("d-none");
+
+  let isPlaying = false;
+
+  playButton.addEventListener("click", function () {
+    togglePlayPause();
+  });
+
+  pauseButton.addEventListener("click", function () {
+    togglePlayPause();
+  });
+
+  function togglePlayPause() {
+    if (isPlaying) {
+      pauseButton.classList.add("d-none");
+      playButton.classList.remove("d-none");
+      audioPlayer.pause();
+    } else {
+      playButton.classList.add("d-none");
+      pauseButton.classList.remove("d-none");
+      audioPlayer.play();
+    }
+
+    isPlaying = !isPlaying;
+  }
 
   audioPlayer.addEventListener("timeupdate", function () {
     const percentage = (audioPlayer.currentTime / audioPlayer.duration) * 100;
@@ -17,6 +44,12 @@ document.addEventListener("DOMContentLoaded", function () {
     totalTime.textContent = formatTime(audioPlayer.duration);
   });
 
+  audioPlayer.addEventListener("ended", function () {
+    pauseButton.classList.add("d-none");
+    playButton.classList.remove("d-none");
+    isPlaying = false;
+  });
+
   // Funzione per formattare i tempi come MM:SS
   function formatTime(time) {
     const minutes = Math.floor(time / 60);
@@ -24,4 +57,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
     return formattedTime;
   }
+});
+
+progressBar.addEventListener("click", function (event) {
+  const barWidth = progressBar.clientWidth;
+  const clickX = event.clientX - progressBar.getBoundingClientRect().left;
+  const percentage = (clickX / barWidth) * 100;
+
+  // Aggiorna il progresso della canzone
+  audioPlayer.currentTime = (percentage / 100) * audioPlayer.duration;
 });
