d5b0209fea8e6ef46fa0af36eea7ad78e424f86d
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-02-11 19:45:51 +0100

    fixed iframe

--- js/generazioneQr.js
+++ js/generazioneQr.js
@@ -5,7 +5,7 @@ function generateQRCode() {
   var options = {
     width: 128,
     height: 128,
-    colorDark: "#000BFF",
+    colorDark: "#E50914",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H,
   };
